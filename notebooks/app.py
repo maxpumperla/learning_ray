5f4557704903c7dc93f5158a2a92ec83e5071f70
Max Pumperla <max.pumperla@googlemail.com>
2022-11-25 12:16:32 +0100

    bulk update
    Signed-off-by: Max Pumperla <max.pumperla@googlemail.com>

--- notebooks/app.py
+++ notebooks/app.py
@@ -58,3 +58,133 @@ class SentimentAnalysis:
 
 
 batched_deployment = SentimentAnalysis.bind()
+
+
+# %%
+# tag::fetch_wikipedia[]
+from typing import Optional
+
+import wikipedia
+
+
+def fetch_wikipedia_page(search_term: str) -> Optional[str]:
+    results = wikipedia.search(search_term)
+    # If no results, return to caller.
+    if len(results) == 0:
+        return None
+
+    # Get the page for the top result.
+    return wikipedia.page(results[0]).content
+# end::fetch_wikipedia[]
+
+
+# %%
+# tag::sentiment_analysis[]
+from ray import serve
+from transformers import pipeline
+from typing import List
+
+
+@serve.deployment
+class SentimentAnalysis:
+    def __init__(self):
+        self._classifier = pipeline("sentiment-analysis")
+
+    @serve.batch(max_batch_size=10, batch_wait_timeout_s=0.1)
+    async def is_positive_batched(self, inputs: List[str]) -> List[bool]:
+        results = self._classifier(inputs, truncation=True)
+        return [result["label"] == "POSITIVE" for result in results]
+
+    async def __call__(self, input_text: str) -> bool:
+        return await self.is_positive_batched(input_text)
+# end::sentiment_analysis[]
+
+
+# %%
+# tag::summarizer[]
+@serve.deployment(num_replicas=2)
+class Summarizer:
+    def __init__(self, max_length: Optional[int] = None):
+        self._summarizer = pipeline("summarization")
+        self._max_length = max_length
+
+    def __call__(self, input_text: str) -> str:
+        result = self._summarizer(
+            input_text, max_length=self._max_length, truncation=True)
+        return result[0]["summary_text"]
+# end::summarizer[]
+
+
+# %%
+# tag::entity_recognition[]
+@serve.deployment
+class EntityRecognition:
+    def __init__(self, threshold: float = 0.90, max_entities: int = 10):
+        self._entity_recognition = pipeline("ner")
+        self._threshold = threshold
+        self._max_entities = max_entities
+
+    def __call__(self, input_text: str) -> List[str]:
+        final_results = []
+        for result in self._entity_recognition(input_text):
+            if result["score"] > self._threshold:
+                final_results.append(result["word"])
+            if len(final_results) == self._max_entities:
+                break
+
+        return final_results
+# end::entity_recognition[]
+
+
+# %%
+# tag::response_model[]
+from pydantic import BaseModel
+
+
+class Response(BaseModel):
+    success: bool
+    message: str = ""
+    summary: str = ""
+    named_entities: List[str] = []
+# end::response_model[]
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@serve.deployment
+@serve.ingress(app)
+class NLPPipelineDriver:
+    def __init__(self, sentiment_analysis, summarizer, entity_recognition):
+        self._sentiment_analysis = sentiment_analysis
+        self._summarizer = summarizer
+        self._entity_recognition = entity_recognition
+
+    @app.get("/", response_model=Response)
+    async def summarize_article(self, search_term: str) -> Response:
+        # Fetch the top page content for the search term if found.
+        page_content = fetch_wikipedia_page(search_term)
+        if page_content is None:
+            return Response(success=False, message="No pages found.")
+
+        # Conditionally continue based on the sentiment analysis.
+        is_positive = await self._sentiment_analysis.remote(page_content)
+        if not is_positive:
+            return Response(success=False, message="Only positivitiy allowed!")
+
+        # Query the summarizer and named entity recognition models in parallel.
+        summary_result = self._summarizer.remote(page_content)
+        entities_result = self._entity_recognition.remote(page_content)
+        return Response(
+            success=True,
+            summary=await summary_result,
+            named_entities=await entities_result
+        )
+
+sentiment_analysis = SentimentAnalysis.bind()
+summarizer = Summarizer.bind()
+entity_recognition = EntityRecognition.bind(threshold=0.95, max_entities=5)
+nlp_pipeline_driver = NLPPipelineDriver.bind(
+    sentiment_analysis, summarizer, entity_recognition)
+
