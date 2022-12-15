c4a55b1ea519b14b17306f8f3bc4fd7748102219
Max Pumperla <max.pumperla@googlemail.com>
2022-12-15 20:54:01 +0100

    maze resources for colab
    Signed-off-by: Max Pumperla <max.pumperla@googlemail.com>

--- notebooks/maze.py
+++ notebooks/maze.py
@@ -1,4 +1,4 @@
 from ray.rllib.algorithms.dqn import DQNConfig
 
 config = DQNConfig().environment("maze_gym_env.GymEnvironment")\
-    .rollouts(num_rollout_workers=2)
+    .rollouts(num_rollout_workers=0)
